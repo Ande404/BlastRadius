9b6647919a25efe6bfb09121f59ca50e9e7250b5
Ande404 <andrewluwaga69@gmail.com>
2026-04-22 17:40:46 +0000

    upload to drive

---
+++ notebooks/config.py
@@ -0,0 +1,42 @@
+from google.colab import drive, userdata
+import os
+import sys
+
+def initialize_project():
+    """
+    Mounts drive, authenticates Git, and sets up system paths.
+    """
+    # 1. Mount Drive
+    if not os.path.exists('/content/drive'):
+      drive.mount('/content/drive')
+
+    # 2. Retrieve Private Secrets
+    try:
+        GIT_TOKEN = userdata.get('BLAST-RADIUS-TOKEN')
+        GIT_NAME = userdata.get('GIT_NAME')
+        REPO_NAME = "BlastRadius"
+        GIT_EMAIL = userdata.get('GIT_EMAIL')
+        PROJECT_ROOT = userdata.get('BLAST_RADIUS_PATH')
+    except userdata.SecretNotFoundError as SNFE:
+        print(f'Error: Add all required KEYS to the Colab Secrets tab first! {SNFE}')
+        return
+    except userdata.NotebookAccessError as NAE:
+        print(f'Error: Unable to access notebook! {NAE}')
+        return
+
+    # 3. Project Paths
+    # Adjust this path if your shared folder name is different
+    # PROJECT_ROOT = f"/content/drive/MyDrive/folder_name/{REPO_NAME}"
+    
+    if PROJECT_ROOT not in sys.path:
+        sys.path.append(os.path.join(PROJECT_ROOT, '/notebooks'))
+    
+    os.chdir(PROJECT_ROOT)
+
+    # 4. Safe Git Configuration
+    os.system(f"git remote set-url origin https://{GIT_TOKEN}@github.com/{GIT_NAME}/{REPO_NAME}.git")
+    os.system(f"git config --global user.email '{GIT_EMAIL}'")
+    os.system(f"git config --global user.name '{GIT_NAME}'")
+
+    print(f"Authenticated as: {GIT_NAME}")
+    print(f"Working Directory: {os.getcwd()}")
